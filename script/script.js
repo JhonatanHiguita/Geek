c655dbfd26482bf6f884d48ac9bf2cb52951f953
Jhonathan Higuita <jhonatanhiguita100@gmail.com>
2021-10-22 22:22:48 -0500

    cambio

--- script/script.js
+++ script/script.js
@@ -1,8 +1,12 @@
 let boton = document.getElementById('Buttom_Suma');
 let boton2 = document.getElementById('Buttom_Resta');
+let boton3 = document.getElementById('Buttom_Multiplicacion');
+let boton4 = document.getElementById('Buttom_Division');
 
 boton.addEventListener('click',capturaSuma);
 boton2.addEventListener('click',capturaResta);
+boton3.addEventListener('click',capturaMultiplicacion);
+boton4.addEventListener('click',capturaDivision);
 
 let form = document.getElementById('form');
 
@@ -30,5 +34,24 @@ function capturaResta(){
     let Resta = numero1 - numero2
     
     console.log(Resta)
+    
+}
+function capturaMultiplicacion(){
+    let numero1 = Number(document.querySelector('#num1').value);
+    let numero2 = Number(document.querySelector('#num2').value);
+
+
+    let Multiplicacion = numero1 * numero2
+    
+    console.log(Multiplicacion)
+}
+function capturaDivision(){
+    let numero1 = Number(document.querySelector('#num1').value);
+    let numero2 = Number(document.querySelector('#num2').value);
+
+
+    let Division = numero1 / numero2
+    
+    console.log(Division)
     
 }
